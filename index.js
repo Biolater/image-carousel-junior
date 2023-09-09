ded4d52457ebbf8bacb597003fd9a1996cd755e0
Biolater <yusifovmurad1@gmail.com>
2023-09-09 10:57:04 +0400

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
-    const button = $("button");
+let counter = 0;
+const counterText = $(".counter-text");
+const increase = $(".increase");
+const decrease = $(".decrease");
 
-    const body = $("body");
-    function  randomNumber(){
-        return Math.floor(Math.random() * 255);
-    }
-    button.click(function () {
-        console.log(randomNumber())
-        let color1 = randomNumber();
-        let color2 = randomNumber();
-        let color3 = randomNumber();
-        body.css("background-color",`rgb(${color1},${color2},${color3})`);
-    })
+counterText.text(`${counter}`)
 
+
+increase.click(function () {
+  counter++;
+  counterText.text(`${counter}`)
+});
+
+decrease.click(function () {
+  counter--;
+  counterText.text(`${counter}`)
 })
