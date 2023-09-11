8871c1cf3b4b68973a0a03ab3c36f81b0ea17298
Biolater <yusifovmurad1@gmail.com>
2023-09-11 13:38:42 +0400

    com

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-const attrs = ["https://miguelznunez.github.io/ImageCarousel/Lam0.jpg", "https://miguelznunez.github.io/ImageCarousel/Lam1.jpg", "https://miguelznunez.github.io/ImageCarousel/Lam2.jpg", "https://miguelznunez.github.io/ImageCarousel/Lam3.jpg", "https://miguelznunez.github.io/ImageCarousel/Lam4.jpg", "https://miguelznunez.github.io/ImageCarousel/Lam5.jpg", "https://miguelznunez.github.io/ImageCarousel/Lam6.jpg", "https://miguelznunez.github.io/ImageCarousel/Lam7.jpg"];
-const prev = $("#prev");
-const next = $("#next");
-let counter = 0;
-const dots = $(".dots span")
-let image = $("img");
-image.attr("src", attrs[counter]);
+let index = 0;
 
-prev.click(function () {
+function showImage(i) {
+  index += i;
+  const images = $(".image");
+  const dots = $(".dots span");
+  images.eq(index).fadeOut(200)
+  images.hide();
   dots.removeClass("active");
-  counter--;
-  if (counter < 0) {
-    counter = attrs.length - 1;
+  if (index > images.length - 1) {
+    index = 0;
   }
-  dots.eq(counter).addClass("active")
-  image.fadeOut(100, function () {
-    image.attr("src", attrs[counter]);
-    image.fadeIn(100);
-  });
-});
-
-next.click(function () {
-  dots.removeClass("active");
-  counter++;
-  if (counter >= attrs.length) {
-    counter = 0;
+  if (index < 0) {
+    index = images.length - 1;
   }
-  dots.eq(counter).addClass("active")
+  images.eq(index).fadeIn(200)
 
-  image.fadeOut(100, function () {
-    image.attr("src", attrs[counter]);
-    image.fadeIn(100);
-  });
+  images.eq(index).show();
+  dots.eq(index).addClass("active");
+}
+
+$(document).ready(function () {
+  showImage(index); // Call showImage function when the document is ready
 });
